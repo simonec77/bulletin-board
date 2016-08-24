e35ecc5728668b68d4cc7f8bcdb011258af91bbb
Lovisa Svallingson <lovisa.svallingson@ca.com>
2016-08-24 12:57:35 +0200

    Sends existing posts, writes posts on quit

--- main.js
+++ main.js
@@ -1,14 +1,57 @@
-import { app, BrowserWindow, ipcMain as ipc } from 'electron';
+import { app, BrowserWindow, ipcMain as ipc, dialog } from 'electron';
+import fs from 'fs';
+
+let mainWindow = null;
+let posts = null;
 
 app.on('ready', () => {
   console.log('The Bulletin Board is starting up...');
 
-  let mainWindow = new BrowserWindow({ width: 1000,
-                                       height: 600,
-                                       frame: false });
+  mainWindow = new BrowserWindow({ width: 1000,
+                                   height: 600,
+                                   frame: false });
+
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    sendExistingPosts();
+  });
+});
+
+ipc.on('data', (event, payload) => {
+  posts = payload;
+});
+
+app.on('before-quit', () => {
+  posts.forEach(writePost);
 });
 
 ipc.on('close', () => {
   app.quit();
 });
+
+const writePost = (post) => {
+  let fileName = `${app.getPath('documents')}/BulletinBoard/${post.id}-note.txt`;
+  fs.writeFileSync(fileName, post.body);
+};
+
+const sendExistingPosts = () => {
+  const path = `${app.getPath('documents')}/BulletinBoard`;
+  ensureDirExists(path);
+
+  let existingPosts = fs.readdirSync(path).map((file, index) => {
+    let content = fs.readFileSync(path + '/' + file).toString();
+    return { id: index, body: content };
+  });
+
+  mainWindow.webContents.send('data', existingPosts);
+};
+
+const ensureDirExists = (path) => {
+  try {
+    fs.mkdirSync(path);
+  } catch (e) {
+    if (e.code !== 'EEXIST') throw e;
+  }
+};
